351129c76c42af92f1a5c24763b86c147312251b
AleKurlat <79816198+AleKurlat@users.noreply.github.com>
2021-09-22 17:56:17 -0300

    modulo

--- codigo.js
+++ codigo.js
@@ -1,3 +1,5 @@
+import { Persona } from "./modulo.js";
+
 const formulario = document.querySelector("#formulario");
 const campoNombre = formulario.querySelector("input[name='nombre']");
 const campoEdad = formulario.querySelector("input[name='edad']");
@@ -5,59 +7,6 @@ const areaPersonas = document.querySelector("#areaPersonas");
 document.querySelector("#botonAgregar").addEventListener("click", agregarPersona);
 
 const personas = [];
-
-class Persona {
-    constructor(nombre, edad) {
-        this.nombre = nombre;
-        this.edad = edad;
-    }
-
-    static crearPersona(nombre, edad) {
-        const nombreOk = this.chequearNombre(nombre);
-        const edadOk = this.chequearEdad(edad);
-        if (nombreOk && edadOk) {
-            return new Persona(nombre, edad);
-        }
-    }
-
-    static chequearNombre(nombre) {
-        if (nombre.length < 6) {
-            alert("El nombre debe tener al menos seis caracteres");
-            return false;
-        }
-        return true;
-    }
-
-    static chequearEdad(edad) {
-        if (typeof edad !== "number") {
-            alert("Debe ingresar un número");
-            return false;
-        }
-        const esMayorOIgualQueCero = edad >= 0;
-        if (!esMayorOIgualQueCero) {
-            alert("El número ingresado debe ser mayor igual a 0");
-            return false;
-        }
-        return true;
-    }
-
-    cambiarNombre(nuevoNombre) {
-        if (Persona.chequearNombre(nuevoNombre)) {
-            this.nombre = nuevoNombre;
-        }
-    }
-
-    cambiarEdad(nuevaEdad) {
-        if (Persona.chequearEdad(nuevaEdad)) {
-            this.edad = nuevaEdad;
-        }
-    }
-
-    obtenerDescripcion() {
-        const descripcion = `Nombre: ${this.nombre}, Edad: ${this.edad}`;
-        return descripcion;
-    }
-}
 
 function agregarPersona(e) {
     const valorNombre = campoNombre.value;
